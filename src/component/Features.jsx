6ad4cbed09ac46110364ff4c6438c199e5a4dd3b
Roihanah Inayati <roihanahinayati@gmail.com>
2025-03-10 20:54:11 +0700

    update features.jsx

--- src/component/Features.jsx
+++ src/component/Features.jsx
@@ -31,20 +31,25 @@ const features = [
 const Features = () => {
   return (
     <section className="flex flex-col items-center px-6 md:px-12 lg:px-36 py-16 bg-[#f4f3ff] font-poppins">
-      <h2 className="text-2xl md:text-3xl font-semibold mb-8">Features</h2>
+      <h2 className="text-2xl md:text-4xl font-semibold mb-16 text-blue-600">
+        Features Tasca
+      </h2>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
         {features.map((feature, index) => (
           <motion.div
             key={index}
-            className="flex flex-col items-start bg-white p-4 rounded-xl shadow-md transition-all duration-500 hover:bg-[#e0e7ff]"
+            className="flex flex-col items-center bg-white p-6 rounded-2xl shadow-lg shadow-gray-200 transition-all duration-500 
+                      hover:bg-gradient-to-br from-blue-100 to-indigo-200"
             whileHover={{ scale: 1.05 }}
           >
-            <img src={feature.icon} alt="icon" className="w-8 h-8 mb-2" />
-            <h3 className="font-semibold text-base md:text-lg mb-1">
+            <div className="p-2 rounded-full mb-3">
+              <img src={feature.icon} alt="icon" className="w-20 h-20" />
+            </div>
+            <h3 className="font-semibold text-lg tracking-wide text-gray-800 mb-2">
               {feature.title}
             </h3>
-            <p className="text-xs md:text-sm text-gray-600">
+            <p className="text-sm text-gray-600 text-center">
               {feature.description}
             </p>
           </motion.div>
