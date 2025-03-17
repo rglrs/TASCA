4600db885a16759c2c9b824f0a7499cc6b64999a
Aldnova <huwaidi373@gmail.com>
2025-03-17 10:18:51 +0700

    Tambah sliding_pages.dart, edit main.dart & pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,30 @@
 import 'package:flutter/material.dart';
-import 'theme.dart';
+import 'sliding_pages.dart'; 
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isDarkMode = false; // State untuk mode gelap
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: lightTheme, // Tema terang
-      darkTheme: darkTheme, // Tema gelap
-      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light, // Mode tema
-      home: MyHomePage(
-        title: 'Flutter Demo Home Page',
-        isDarkMode: isDarkMode,
-        onThemeChanged: (value) {
-          setState(() {
-            isDarkMode = value;
-          });
-        },
+      title: 'Sliding Pages Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-  final bool isDarkMode;
-  final ValueChanged<bool> onThemeChanged;
-
-  const MyHomePage({
-    super.key,
-    required this.title,
-    required this.isDarkMode,
-    required this.onThemeChanged,
-  });
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        actions: [
-          Switch(value: widget.isDarkMode, onChanged: widget.onThemeChanged),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/background.png'), // Path to your PNG image
+              fit: BoxFit.cover, // Cover the entire screen
             ),
-          ],
+          ),
+          child: SlidingPages(), // Set SlidingPages as the home widget
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
