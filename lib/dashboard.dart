517c5745d753c37f97bc2a77064d5d1e68b79a08
rglrs <rglridho123@gmail.com>
2025-03-19 10:38:05 +0700

    login register

---
+++ lib/dashboard.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class DashboardPage extends StatelessWidget {
+  final String userName;
+
+  DashboardPage({required this.userName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () {
+              // Navigate back to the login page
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => LoginPage()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome, $userName!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'This is your dashboard.',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
