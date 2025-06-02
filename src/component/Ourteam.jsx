96ac660a2300cd08d5aa4f313cc533983fea3fe3
Alfani22 <marietanalfani@gmail.com>
2025-06-02 15:39:48 +0700

    perbaikan ourteam

--- src/component/Ourteam.jsx
+++ src/component/Ourteam.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
+const bgImage =
+  "https://res.cloudinary.com/dqci5udgl/image/upload/v1748190761/OT_bg_2_dzlc9a.png";
 
 const teamMembers = [
   {
@@ -43,8 +45,16 @@ const teamMembers = [
 
 const OurTeam = () => {
   return (
-    <div>
-      <h2 className="text-3xl font-bold text-black mb-10 text-center">Our Team</h2>
+    <div
+      className="pt-20 pb-10 py-20 text-center px-4 sm:px-6 lg:px-8 min-h-screen h-full bg-no-repeat bg-top"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "top center",
+      }}
+    >
+      <h2 className="text-3xl font-bold text-black mb-10">Our Team</h2>
 
       <div className="max-w-5xl mx-auto space-y-8 px-4 sm:px-0">
         {/* Baris pertama */}
@@ -96,8 +106,9 @@ const OurTeam = () => {
               <motion.div
                 key={index}
                 ref={ref}
-                className={`flex flex-col items-center ${index === 2 ? "col-span-2 sm:col-span-1 mx-auto" : ""
-                  }`}
+                className={`flex flex-col items-center ${
+                  index === 2 ? "col-span-2 sm:col-span-1 mx-auto" : ""
+                }`}
                 initial={{ opacity: 0, y: 30 }}
                 animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 30 }}
                 transition={{ duration: 0.5, delay: (index + 4) * 0.1 }}
